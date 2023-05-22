451f5f780c728e5a3a16acde1c3926983ad3bf72
jaewoo_byeon2 <jaewoo_byeon@tmax.co.kr>
2023-05-22 19:02:14 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,128 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  animate,
+  AnimatePresence,
+  useMotionValue,
+  MotionConfig,
+  type PanInfo,
+  motion,
+} from "framer-motion";
+import { useState } from "react";
+
+const COLORS: string[] = [
+  "bg-red-500",
+  "bg-yellow-500",
+  "bg-green-500",
+  "bg-blue-500",
+  "bg-pink-500",
+];
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const x = useMotionValue(0);
+
+  function handlePrev() {
+    if (index > 0) {
+      const newIndex = index - 1;
+      setIndex(newIndex);
+      animate(x, newIndex * -100);
+    }
+  }
+
+  function handleNext() {
+    if (index < COLORS.length - 1) {
+      setIndex(index + 1);
+    }
+  }
+
+  function onPan(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) {
+    // move the element horizontally according to cursor position calculated from `-index * 100%`
+    // and the offset of the cursor from the initial position
+    const OFFSET = 100;
+    const x = `-${index * 100 + info.offset.x / 10}%`;
+  }
+
+  function onPanEnd(
+    event: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo
+  ) {
+    const OFFSET = 100;
+    if (info.offset.x > OFFSET) {
+      handlePrev();
+    } else if (info.offset.x < OFFSET) {
+      handleNext();
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-center bg-yellow-100 p-24">
+      <MotionConfig
+        transition={{
+          duration: 1,
+          ease: [0.32, 0.72, 0, 1],
+        }}
+      >
+        <section className="relative overflow-hidden rounded-md bg-yellow-50 shadow-xl shadow-yellow-900/10">
+          <div className="flex w-64">
+            <div className="grid w-full grid-cols-2">
+              <AnimatePresence initial={false}>
+                {index > 0 && (
+                  <motion.button
+                    className="col-start-1 p-4"
+                    onClick={handlePrev}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 0.6 }}
+                    exit={{ opacity: 0, pointerEvents: "none" }}
+                    whileHover={{ opacity: 1 }}
+                  >
+                    Prev
+                  </motion.button>
+                )}
+              </AnimatePresence>
+              <AnimatePresence initial={false}>
+                {index < COLORS.length - 1 && (
+                  <motion.button
+                    className="col-start-2 p-4"
+                    onClick={handleNext}
+                    animate={{ opacity: 0.6 }}
+                    exit={{ opacity: 0, pointerEvents: "none" }}
+                    whileHover={{ opacity: 1 }}
+                  >
+                    Next
+                  </motion.button>
+                )}
+              </AnimatePresence>
+            </div>
+          </div>
+          <motion.div
+            className="flex h-64 w-64"
+            animate={{
+              x: `-${index * 100}%`,
+            }}
+            onPan={onPan}
+            onPanEnd={onPanEnd}
+
+            // drag="x"
+            // dragControls={dragControls}
+            // dragConstraints={{ left: 0, right: 0 }}
+            // dragElastic={0.1}
+            // dragMomentum={false}
+            // onDragEnd={(event, info) => {
+            //   const OFFSET = 100;
+            //   if (info.offset.x > OFFSET) {
+            //     handlePrev();
+            //   } else if (info.offset.x < OFFSET) {
+            //     handleNext();
+            //   }
+            // }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            {COLORS.map((color) => (
+              <div key={color} className={`${color} h-full w-full shrink-0`} />
+            ))}
+          </motion.div>
+        </section>
+      </MotionConfig>
     </main>
-  )
+  );
 }
